6053afbce42d4ea8014ad000af7d68312c9f8867
waggisto <67224668+waggisto@users.noreply.github.com>
2020-09-08 14:30:00 +0200

    S

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Meleko Grocery Shopping
+       Going somewhere Grocery Shopping
       </header>
     </div>
   );
